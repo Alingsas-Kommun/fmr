020dcf87d47c68c2a53649024bca5a934fa7f882
Adam Alexandersson <adam.alexandersson@consid.se>
2025-09-18 00:42:02 +0200

    Update content-single-board.blade.php

--- resources/views/partials/post-types/content-single-board.blade.php
+++ resources/views/partials/post-types/content-single-board.blade.php
@@ -137,7 +137,7 @@
                             </td>
                             
                             <td class="px-6 py-4">
-                                <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 dark:bg-gray-200 text-gray-800 dark:text-gray-100">
+                                <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-emerald-100 text-emerald-800">
                                     {{ $authority->type }}
                                 </span>
                             </td>
